5accb8a4937ba1ea3523fb36bc1b4e21d2ce945b
Karan Dangol <101792458+karan-0dangol@users.noreply.github.com>
2022-03-17 13:59:45 +0545

    Add files via upload

---
+++ calculator/c.js
@@ -0,0 +1,44 @@
+let display = document.querySelector('.display')
+
+let btn = Array.from(document.getElementsByClassName('button'))
+
+
+btn.forEach(btn=>{
+    btn.addEventListener('click', e=>{
+
+
+        
+        switch(e.target.innerText)
+       { case 'C':
+            display.innerText = ''
+            break
+
+        case '←':
+            if(display.innerText){display.innerText = display.innerText.slice(0,-1)}
+             break
+
+        case '=':
+            try {
+                display.innerText = eval(display.innerText)
+            } catch  {
+                display.innerText = "Error!"
+                setTimeout(() => {
+                    display.innerText = ''
+                }, 1500);
+                
+               
+            }
+            
+            break
+
+       
+
+        default:
+            display.innerText += e.target.innerText
+        
+        }
+
+        
+            
+    })
+})
